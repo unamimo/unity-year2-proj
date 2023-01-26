43b85aff9b6d87ab5659eb6daae32563c03005fb
13rycartwright <c0015856@my.shu.ac.uk>
2023-01-26 19:35:42 +0000

    - Photo moved to bottom left corner of the screen - Photo can no longer be taken whilst another is held up - Camera now produces a flash effect when left click is pressed, which also shows up on and brightens the photo

--- unity_year2/Assets/Scripts/PolaroidBehaviour.cs
+++ unity_year2/Assets/Scripts/PolaroidBehaviour.cs
@@ -19,6 +19,11 @@ public class PolaroidBehaviour : MonoBehaviour
     [SerializeField]
     private Image photoDisplayArea;
 
+    [SerializeField]
+    private GameObject cameraFlash;
+    [SerializeField]
+    private float flashTime;
+
     private void OnEnable()
     {
         takePhotoInput.Enable();
@@ -37,6 +42,7 @@ public class PolaroidBehaviour : MonoBehaviour
         capture = new Texture2D(Screen.width, Screen.height, TextureFormat.RGB24, false);
         photoDisplayArea.GetComponent<RectTransform>().anchoredPosition = new Vector2(0, -1000);
         _gameController = GameObject.Find("GameManager").GetComponent<GameController>();
+        cameraFlash.SetActive(false);
     }
 
     // Update is called once per frame
@@ -44,34 +50,44 @@ public class PolaroidBehaviour : MonoBehaviour
     {
         if (_gameController.GetState() != GameController.EGameState.Playing)
             return;
-        if (takePhotoInput.triggered)
-        {
+        if ((takePhotoInput.triggered) && !(holdUpPhoto.ReadValue<float>() > 0.1f))
+            {
             UnityEngine.Debug.Log("yo");
             StartCoroutine(TakePhoto());
         }
 
         if (holdUpPhoto.ReadValue<float>() > 0.1f)
         {
-            photoDisplayArea.GetComponent<RectTransform>().anchoredPosition = new Vector2(0, 0);
-        }
+            photoDisplayArea.GetComponent<RectTransform>().anchoredPosition = new Vector2(-Screen.width/2 + 320, -Screen.height/2 + 330);
+        }                                                                           //hacked for now, 306 = size of polaroid image / 2
         else
         {
             photoDisplayArea.GetComponent<RectTransform>().anchoredPosition = new Vector2(0, -1000);
         }
     }
 
+    IEnumerator CameraFlashEffect()
+    {
+        cameraFlash.SetActive(true);
+        yield return new WaitForSeconds(flashTime);
+        cameraFlash.SetActive(false);
+    }
+
     IEnumerator TakePhoto()
     {
+        cameraFlash.SetActive(true);
         yield return new WaitForEndOfFrame();
-
         Rect regionToRead = new Rect(0, 0, Screen.width, Screen.height);
         capture.ReadPixels(regionToRead, 0, 0, false);
         capture.Apply();
         ShowPhoto();
+        yield return new WaitForSeconds(flashTime);
+        cameraFlash.SetActive(false);
     }
 
     void ShowPhoto()
     {
+        //StartCoroutine(CameraFlashEffect());
         Sprite photoSprite = Sprite.Create(capture, new Rect(0.0f, 0.0f, capture.width, capture.height), new Vector2(0.5f, 0.5f), 100.0f);
         photoDisplayArea.sprite = photoSprite;
     }
