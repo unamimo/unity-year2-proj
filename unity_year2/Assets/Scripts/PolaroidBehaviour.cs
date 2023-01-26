6df4bfd2773a45720e0a44b26d2869a6da5006bf
LAPTOP-K41BCEBF\Pauls <c0015856@my.shu.ac.uk>
2023-01-26 15:10:43 +0000

    - Any photo taken will now be offscreen by default - Pressing Q will display the most recently taken photo to the screen - Photos can no longer be taken in the main menu

--- unity_year2/Assets/Scripts/PolaroidBehaviour.cs
+++ unity_year2/Assets/Scripts/PolaroidBehaviour.cs
@@ -11,7 +11,9 @@ using UnityEngine.UI;
 
 public class PolaroidBehaviour : MonoBehaviour
 {
+    private GameController _gameController;
     public InputAction takePhotoInput;
+    public InputAction holdUpPhoto;
     Texture2D capture;
 
     [SerializeField]
@@ -20,26 +22,41 @@ public class PolaroidBehaviour : MonoBehaviour
     private void OnEnable()
     {
         takePhotoInput.Enable();
+        holdUpPhoto.Enable();
     }
 
     private void OnDisable()
     {
         takePhotoInput.Disable();
+        holdUpPhoto.Disable();
     }
 
     // Start is called before the first frame update
     void Start()
     {
         capture = new Texture2D(Screen.width, Screen.height, TextureFormat.RGB24, false);
+        photoDisplayArea.GetComponent<RectTransform>().anchoredPosition = new Vector2(0, -1000);
+        _gameController = GameObject.Find("GameManager").GetComponent<GameController>();
     }
 
     // Update is called once per frame
     void Update()
     {
+        if (_gameController.GetState() != GameController.EGameState.Playing)
+            return;
         if (takePhotoInput.triggered)
         {
             UnityEngine.Debug.Log("yo");
             StartCoroutine(TakePhoto());
+        }
+
+        if (holdUpPhoto.ReadValue<float>() > 0.1f)
+        {
+            photoDisplayArea.GetComponent<RectTransform>().anchoredPosition = new Vector2(0, 0);
+        }
+        else
+        {
+            photoDisplayArea.GetComponent<RectTransform>().anchoredPosition = new Vector2(0, -1000);
         }
     }
 
