bb7913e9cc729624645eab014b966ea6f03b26bd
13rycartwright <c0015856@my.shu.ac.uk>
2023-02-02 13:41:49 +0000

    small fix

--- unity_year2/Assets/Scripts/DoorInteract.cs
+++ unity_year2/Assets/Scripts/DoorInteract.cs
@@ -2,6 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using System.Security.Cryptography;
 using UnityEngine;
+using UnityEngine.InputSystem;
 
 public class DoorInteract : MonoBehaviour
 {
@@ -10,6 +11,17 @@ public class DoorInteract : MonoBehaviour
     bool active = false;
     private Inventory inventory;
     public GameObject _player;
+    public InputAction openDoor;
+
+    private void OnEnable()
+    {
+        openDoor.Enable();
+    }
+
+    private void OnDisable()
+    {
+        openDoor.Disable();
+    }
 
     private void Start()
     {
@@ -21,7 +33,7 @@ public class DoorInteract : MonoBehaviour
         RaycastHit hit;
         active = Physics.Raycast(cam.position, cam.TransformDirection(Vector3.forward), out hit, ActivationDistance);
 
-        if (Input.GetKeyDown(KeyCode.E) && active == true)
+        if (openDoor.triggered && active == true)
         {
             if(gameObject.tag == "GreenKey")
             {
