9f43d65de197fe6634e1c547cc242e082df7759b
LAPTOP-K41BCEBF\Pauls <c0015856@my.shu.ac.uk>
2023-02-14 16:34:13 +0000

    - Added new sounds for the breaking platform

--- unity_year2/Assets/Scripts/BreakingPlatform.cs
+++ unity_year2/Assets/Scripts/BreakingPlatform.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -7,6 +8,7 @@ public class BreakingPlatform : MonoBehaviour
 
     [SerializeField]
     private float breakTime;
+    private int soundIndex = 1;
 
     // Start is called before the first frame update
     void Start()
@@ -33,7 +35,27 @@ public class BreakingPlatform : MonoBehaviour
         Debug.Log("Waiting...");
         yield return new WaitForSeconds(breakTime);
         Debug.Log("Drop");
-        FindObjectOfType<AudioControl>().Play("Fall", false);
+        soundIndex = UnityEngine.Random.Range(1, 6);
+        switch (soundIndex)
+        {
+            case 1:
+                FindObjectOfType<AudioControl>().Play("Thud1", false);
+                break;
+            case 2:
+                FindObjectOfType<AudioControl>().Play("Thud2", false);
+                break;
+            case 3:
+                FindObjectOfType<AudioControl>().Play("Thud3", false);
+                break;
+            case 4:
+                FindObjectOfType<AudioControl>().Play("Thud4", false);
+                break;
+            case 5:
+                FindObjectOfType<AudioControl>().Play("Thud5", false);
+                break;
+            default:
+                break;
+        }
         gameObject.SetActive(false);
     }
 }
